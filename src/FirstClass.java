3bea5525314936f5c163c87ac0cbf45cdcac12d1
hdiab22 <42975537+HadyTheDiabMan@users.noreply.github.com>
2018-09-13 12:26:21 -0400

    Add files via upload

---
+++ src/FirstClass.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @author Hady Diab 
+ * First Program
+ *
+ */
+
+public class FirstClass 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.print("hello, everyone");
+
+	}
+
+}
