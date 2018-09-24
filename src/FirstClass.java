b9cd8006b8e73223aff2ea3155c54aac1c0e6399
hdiab22 <42975537+HadyTheDiabMan@users.noreply.github.com>
2018-09-24 11:25:54 -0400

    Update FirstClass.java

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -10,7 +10,7 @@ public class FirstClass
 
 	public static void main(String[] args) 
 	{
-		System.out.print("hello, everyone");
+		System.out.print("hello, I have just submitted by first project into github");
 
 	}
 
